8334ac6a8b7a7544205a581677209eaf7f4be0a7
Mark O'Neill <mto@byu.edu>
2017-10-19 15:14:45 -0600

    Adding test for listen

--- test_files/sock_opts_tests/simple_tests.c
+++ test_files/sock_opts_tests/simple_tests.c
@@ -12,13 +12,16 @@
 
 void run_sockops_tests(void);
 void run_connect_tests(void);
+void run_listen_tests(void);
 void run_hostname_tests(void);
 int connect_to_host(char* host, char* service);
 int connect_to_host_new(char* host, char* service);
+int create_server_socket(char* port, int protocol);
 
 int main(int argc, char* argv[]) {
 	run_sockops_tests();
 	//run_hostname_tests();
+	run_listen_tests();
 	run_connect_tests();
 	printf("All tests succeeded!\n");
 	return 0;
@@ -62,6 +65,11 @@ void run_sockops_tests(void) {
 		perror("connect");
 		exit(EXIT_FAILURE);
 	}
+	return;
+}
+
+void run_listen_tests(void) {
+	int sock_fd = create_server_socket("3333", SOCK_STREAM);
 	return;
 }
 
@@ -153,3 +161,78 @@ int connect_to_host_new(char* host, char* service) {
 	return sock;
 }
 
+int create_server_socket(char* port, int protocol) {
+	int sock;
+	int ret;
+	int optval = 1;
+	struct addrinfo hints;
+	struct addrinfo* addr_ptr;
+	struct addrinfo* addr_list;
+
+	memset(&hints, 0, sizeof(hints));
+	hints.ai_family = AF_UNSPEC;
+	hints.ai_socktype = protocol;
+	/* AI_PASSIVE for filtering out addresses on which we
+	 * can't use for servers
+	 *
+	 * AI_ADDRCONFIG to filter out address types the system
+	 * does not support
+	 *
+	 * AI_NUMERICSERV to indicate port parameter is a number
+	 * and not a string
+	 *
+	 * */
+	hints.ai_flags = AI_PASSIVE | AI_ADDRCONFIG | AI_NUMERICSERV;
+	/*
+	 *  On Linux binding to :: also binds to 0.0.0.0
+	 *  Null is fine for TCP, but UDP needs both
+	 *  See https://blog.powerdns.com/2012/10/08/on-binding-datagram-udp-sockets-to-the-any-addresses/
+	 */
+	ret = getaddrinfo(protocol == SOCK_DGRAM ? "::" : NULL, port, &hints, &addr_list);
+	if (ret != 0) {
+		fprintf(stderr, "Failed in getaddrinfo: %s\n", gai_strerror(ret));
+		exit(EXIT_FAILURE);
+	}
+	
+	for (addr_ptr = addr_list; addr_ptr != NULL; addr_ptr = addr_ptr->ai_next) {
+		sock = socket(addr_ptr->ai_family, addr_ptr->ai_socktype, IPPROTO_TLS);
+		if (sock == -1) {
+			perror("socket");
+			continue;
+		}
+
+		// Allow us to quickly reuse the address if we shut down (avoiding timeout)
+		ret = setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, &optval, sizeof(optval));
+		if (ret == -1) {
+			perror("setsockopt");
+			close(sock);
+			continue;
+		}
+
+		ret = bind(sock, addr_ptr->ai_addr, addr_ptr->ai_addrlen);
+		if (ret == -1) {
+			perror("bind");
+			close(sock);
+			continue;
+		}
+		break;
+	}
+	freeaddrinfo(addr_list);
+	if (addr_ptr == NULL) {
+		fprintf(stderr, "Failed to find a suitable address for binding\n");
+		exit(EXIT_FAILURE);
+	}
+
+	if (protocol == SOCK_DGRAM) {
+		return sock;
+	}
+	// Turn the socket into a listening socket if TCP
+	ret = listen(sock, SOMAXCONN);
+	if (ret == -1) {
+		perror("listen");
+		close(sock);
+		exit(EXIT_FAILURE);
+	}
+
+	return sock;
+}
