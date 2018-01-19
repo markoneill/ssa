70cfe0cc9f1021de3df2e3dec65faa8c8c815582
Mark O'Neill <mto@byu.edu>
2018-01-18 17:25:32 -0700

    Doing some touchups on the threaded test

--- test_files/https_client/threaded_client.c
+++ test_files/https_client/threaded_client.c
@@ -10,38 +10,58 @@
 #include <pthread.h>
 #include "../../socktls.h"
 
+typedef struct param {
+	int id; /* thread ID */
+	/* add other things here as needed */
+} param_t;
+
 int connect_to_host(char* host, char* service);
 void * threaded_connection();
 void recv_func(int sock_fd,char* http_response);
 void send_func(int sock_fd, char* http_request,int len);
-int const CALLS_PER_THREAD = 2;
+int const CALLS_PER_THREAD = 2000;
 int const NUM_THREADS = 10;
+
 int main() {
-	signal(SIGPIPE, SIG_IGN);
-    pthread_t t[NUM_THREADS];
-	for(int i = 0; i < NUM_THREADS ; i++){
-        pthread_create(&t[i], NULL, threaded_connection,NULL);
-    }
-    for(int i = 0; i < NUM_THREADS; i++){
-        pthread_join(t[i],NULL);
-    }
-    return 0;
+	int i;
+	pthread_t t[NUM_THREADS];
+	param_t t_params[NUM_THREADS];
+
+	signal(SIGPIPE, SIG_IGN); /* Non-portable but I don't care right now */
+
+	for(i = 0; i < NUM_THREADS ; i++) {
+		t_params[i].id = i;
+		pthread_create(&t[i], NULL, threaded_connection, (void*)&t_params[i]);
+	}
+	for(i = 0; i < NUM_THREADS; i++) {
+		pthread_join(t[i],NULL);
+	}
+	return 0;
 }
-void * threaded_connection(){
-    int sock_fd = connect_to_host("www.phoenixteam.net", "443");
-	char http_request[] = "GET / HTTP/1.1\r\nHost: www.phoenixteam.net\r\n\r\n";
-	
+
+void * threaded_connection(void* arg) {
+	int i;
+	int sock_fd;
+	int thread_id;
+	param_t* params = (param_t*)arg;
 	char http_response[2048];
+	char http_request[] = "GET / HTTP/1.1\r\nHost: www.phoenixteam.net\r\n\r\n";
+	thread_id = params->id;
+
+	sock_fd = connect_to_host("www.phoenixteam.net", "443");
+
 	memset(http_response, 0, 2048);
-    for(int i = 0; i < CALLS_PER_THREAD; i++){
-	    send_func(sock_fd, http_request,sizeof(http_request)-1);
-	    recv_func(sock_fd, http_response);
-	    printf("Iteration %d completed for thread ID %ld\n", i, pthread_self());
-	//printf("Received:\n%s", http_response);
-    }
+	for(i = 0; i < CALLS_PER_THREAD; i++) {
+		send_func(sock_fd, http_request,sizeof(http_request)-1);
+		recv_func(sock_fd, http_response);
+		//printf("Iteration %d completed for thread ID %d\n", i, thread_id);
+		//printf("Received:\n%s", http_response);
+	}
+	printf("Thread %d finished %d iterations\n", thread_id, i);
 	close(sock_fd);
-    return NULL;
+	return NULL;
 }
+
 void send_func(int sock_fd, char* http_request,int len) {
 	send(sock_fd, http_request, len, 0);
 }
