40afce215e28aeaff59ce610c5c299f99a4f5ee7
Mark O'Neill <mto@byu.edu>
2018-02-26 16:13:50 -0700

    Syncing TLS socket macros with tls wrapper

--- socktls.h
+++ socktls.h
@@ -1,25 +1,37 @@
-#ifndef SOCKTLS_H
-#define SOCKTLS_H
+#ifndef IN_TLS
+#define IN_TLS
 
-#define IPPROTO_TLS 	(715 % 255)
+/* Protocol */
+#define IPPROTO_TLS     (715 % 255)
 
-#define SO_HOSTNAME             85
-#define SO_PEER_CERTIFICATE     86
-#define SO_CERTIFICATE_CHAIN    87
-#define SO_PRIVATE_KEY          88
-#define SO_ID		        89
+/* Options */
+#define SO_HOSTNAME                      85
+#define SO_PEER_SNI                      86
+#define SO_TRUSTED_PEER_CERTIFICATES     87
+#define SO_CERTIFICATE_CHAIN             88
+#define SO_PRIVATE_KEY                   89
+#define SO_ALPN                          90
+#define SO_SESSION_TTL                   91
+#define SO_DISABLE_CIPHER                92
+#define SO_PEER_CERTIFICATE              93
+#define SO_ID                            94
 
+/* TCP options */
+#define TCP_UPGRADE_TLS         33
 
-#define AF_HOSTNAME	43
+/* Address types */
+#define AF_HOSTNAME     43
 
-struct host_addr { 
-        unsigned char name[255]; 
-}; 
- 
-struct sockaddr_host { 
-        sa_family_t sin_family; 
-        unsigned short sin_port; 
-        struct host_addr sin_addr; 
-}; 
+struct host_addr {
+        unsigned char name[255];
+};
+
+struct sockaddr_host {
+        sa_family_t sin_family;
+        unsigned short sin_port;
+        struct host_addr sin_addr;
+};
+
 
 #endif
+
